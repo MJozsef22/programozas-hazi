7f166c5976bb3818c7574142ab604e3eb9260a77
MJozsef22 <68011008+MJozsef22@users.noreply.github.com>
2021-12-06 18:28:38 +0100

    class beolvasás

---
+++ file io/FileRead.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_io
+{
+    class FileRead
+    {
+        public string nev { get; private set; }
+        public string datum { get; private set; }
+        public string varos { get; private set; }
+        public string eletkor { get; private set; }
+        public string anyjaNeve { get; private set; }
+
+        public FileRead(string line)
+        {
+            string[] datas = line.Split(';');
+            nev = datas[0];
+            datum = datas[1];
+            varos = datas[2];
+            eletkor = datas[3];
+            anyjaNeve = datas[4];
+        }
+        
+    }
+}
---
+++ file io/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file_io
+{
+    class Program
+    {
+        static List<FileRead> list = new List<FileRead>();
+
+        static void Main(string[] args)
+        {
+            
+
+            string path = "datas.txt";
+            if (File.Exists(path))
+            {
+                string[] temp = File.ReadAllLines(path);
+                foreach (string sor in temp)
+                {
+                    FileRead data = new FileRead(sor);
+                    list.Add(data);
+                }
+
+                kiiras();
+            }
+            else
+            {
+                Console.WriteLine("A megadaott fájl nem találjóható a mappájában!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void kiiras()
+        {
+            foreach (var item in list)
+            {
+
+                Console.WriteLine("===============================");
+                Console.WriteLine("Név: {0}", item.nev);
+                Console.WriteLine("Dátum: {0} ", item.datum);
+                Console.WriteLine("Város: {0} ", item.varos);
+                Console.WriteLine("Életkor: {0} ", item.eletkor);
+                Console.WriteLine("Anyja neve: {0} ", item.anyjaNeve);
+                Console.WriteLine("===============================");
+            }
+        }
+    }
+}
