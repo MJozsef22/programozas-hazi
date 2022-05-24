799b48acef862320c5d991e08d66cba03cbb00ac
Mjozsef <68011008+MJozsef22@users.noreply.github.com>
2022-05-24 20:17:06 +0200

    doga

---
+++ SuperBowl/SuperBowl/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBowl
+{
+    internal class Program
+    {
+        static List<Read> data = new List<Read>();
+        static void Main(string[] args)
+        {
+            if (File.Exists("SuperBowl.txt"))
+            {
+                string[] temp = File.ReadAllLines("SuperBowl.txt");
+                foreach (string line in temp.Skip(1))
+                    data.Add(new Read(line));
+
+                Console.WriteLine($"4.feladat:{data.Count}db");
+                List<double> eredmeny1 = new List<double>();
+                List<double> gyoztes = new List<double>();
+                List<double> vesztes = new List<double>();
+
+                foreach (var item in data)
+                {
+                    string[] splitedData = item.eredmeny.Split('-');
+                    gyoztes.Add(int.Parse(splitedData[0]));
+                    vesztes.Add(int.Parse(splitedData[1]));
+                    eredmeny1.Add(int.Parse(splitedData[0]) - int.Parse(splitedData[1]));
+                }
+                //Console.WriteLine(String.Join(",",eredmeny1));
+                double osszes = 0;
+                foreach (var item in eredmeny1)
+                {
+                    osszes += item;
+                }
+
+                double average = Math.Round((osszes / data.Count), 1);
+                Console.WriteLine($"5.feladat: {average}");
+                int max = int.MinValue;
+                int srszam = 0;
+                int srszamVegleges = 0;
+                foreach (var item in data)
+                {
+                    srszam++;
+                    if (max < item.nezok)
+                    {
+                        max = item.nezok;
+                        srszamVegleges = srszam;
+                    }
+                }
+                List<RomaiSorszam> sorszam = new List<RomaiSorszam>();
+                List<string> sorszam2 = new List<string>();
+                foreach (var item in data)
+                {
+                    sorszam.Add(new RomaiSorszam(item.sorszam));
+                }
+                foreach (var item in sorszam)
+                {
+                    sorszam2.Add(item.ArabSsz);
+                }
+                foreach (var item in data)
+                {
+                    if (item.nezok == max)
+                    {
+                        Console.WriteLine($"6.feladat :\n"
+                            + $"Sorszam(dátum): {srszamVegleges} ({item.datum})\n"
+                            + $"Győztes csapat: {item.gyoztes}, szerzett pontok: {gyoztes[srszamVegleges-1]}\n"
+                            + $"Vesztes csapat: {item.vesztes}, szerzett pontok: {vesztes[srszamVegleges-1]}\n"
+                            + $"Helyszín: {item.helyszin}\n"
+                            + $"Város, Állam: {item.Varos_Allam}\n"
+                            + $"Nezőszám: {max}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("A fájl nem található!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SuperBowl/SuperBowl/Read.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBowl
+{
+    internal class Read
+    {
+        public string sorszam { get; private set; }
+        public string datum { get; private set; }
+        public string gyoztes { get; private set; }
+        public string eredmeny{ get; private set; }
+        public string vesztes { get; private set; }
+        public string helyszin { get; private set; }
+        public string Varos_Allam { get; private set; }
+        public int nezok { get; private set; }
+
+        public Read(string line)
+        {
+            string[] temp = line.Split(';');
+            sorszam = temp[0];
+            datum = temp[1];
+            gyoztes = temp[2];
+            eredmeny = temp[3];
+            vesztes = temp[4];
+            helyszin = temp[5];
+            Varos_Allam = temp[6];
+            nezok = int.Parse(temp[7]);
+        }
+    }
+}
